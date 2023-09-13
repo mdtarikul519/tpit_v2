6c8b66c78f13f46219436e33fac5c8a7aac63196
tarikul islam <tarikulmd519@gmail.com>
2023-09-13 22:00:17 +0600

    course migration

---
+++ app/Models/Course/CourseModulAtAGlances.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Course;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CourseModulAtAGlances extends Model
+{
+    use HasFactory;
+}
