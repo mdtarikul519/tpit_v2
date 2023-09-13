4cdadef84d2e0a3e30976530d5a27b942378678c
Md-shefat-masum <mshefat924@gmail.com>
2023-09-13 10:14:05 +0600

    project init

---
+++ tsc.ts
@@ -0,0 +1,21 @@
+console.log('tsc test');
+
+var setting = {
+    logo: '',
+    email: '',
+    mobile_number: [],
+    address: '',
+    map: '',
+    social_link: [
+        {
+            title: '',
+            link: '',
+        },
+        {
+            title: '',
+            link: '',
+        },
+    ],
+    youtubel_link: '',
+    mobile_number2: '',
+}
